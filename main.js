fa17988df15661d0a69b6bc74b0af8a37374f396
Khorapinese <khorapin.pingadpu@mail.kmutt.ac.th>
2021-02-13 02:22:56 +0700

    Last work

--- main.js
+++ main.js
@@ -3,23 +3,62 @@ const app = {
         return {
             msg: 'Hello, Vue3',
 
-            photos: [{image: 'images/1.jpeg',article: 'Hello',done: false},
-            {image: 'images/2.jpg',article: 'World' ,done: false},
-            {image: 'images/3.jpeg',article: 'Kushina' ,done: false},
-            {image: 'images/4.jpeg',article: 'Minato' ,done: false}
+         photos: [
+            {
+                src: 'images/2.jpg',
+                article: 'Hello',
+                favor: false,
+                like: 0,
+                done: false
+            },
+            {
+                src: 'images/5.jpg',
+                article: 'World',
+                favor: false,
+                like: 0,done: false
+            },
+            {
+                src: 'images/6.jpg',
+                article: 'Dolphin',
+                favor: false,
+                like: 0,done: false
+
+            },
+            {
+                src: 'images/9.jpg',
+                article: 'Team 2020',
+                favor: false,
+                like: 0,done: false
+            },
+            {
+                src: 'images/8.jpg',
+                article: 'Welcome to ',
+                favor: false,
+                like: 0,done: false
+            },
+            {
+                src: 'images/11.jpg',
+                article: 'this Family <3',
+                favor: false,
+                like: 0,done: false
+            }
+      
+
         ]
-        }
+    }
     },
     methods: {
         clickHeart(index){
             this.photos[index].done = !this.photos[index].done
         }
+     
     },
     computed: {
         unclickHeart(){
             return this.photos.filter( t => !t.false ).length
         }
     }
+ 
 
 }
 Vue.createApp(app).mount('#app')
